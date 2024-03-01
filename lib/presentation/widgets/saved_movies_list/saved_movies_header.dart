11b135c4e90ee60faf8cc581085dfe3a771e17a9
Balazs Holpar <balazs02.holpar@gmail.com>
2024-03-01 10:29:37 +0100

    Added logout button to watchlist header

--- lib/presentation/widgets/saved_movies_list/saved_movies_header.dart
+++ lib/presentation/widgets/saved_movies_list/saved_movies_header.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movie_finder/presentation/bloc/auth/auth_bloc.dart';
+import 'package:movie_finder/presentation/bloc/auth/auth_event.dart';
 
 class SavedMoviesHeader extends StatelessWidget {
   const SavedMoviesHeader({super.key, required this.username});
@@ -9,11 +12,26 @@ class SavedMoviesHeader extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Hello $username!", style: Theme.of(context).textTheme.headlineMedium!.copyWith(fontWeight: FontWeight.bold)),
-        const SizedBox(height: 8),
-        Text("You can see and manage your watchlist here", style: Theme.of(context).textTheme.headlineSmall,)
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Hello $username!", style: Theme.of(context).textTheme.headlineSmall!.copyWith(fontWeight: FontWeight.bold)),
+            MaterialButton(
+              highlightColor: Theme.of(context).colorScheme.outlineVariant,
+              splashColor: Theme.of(context).colorScheme.outlineVariant,
+              onPressed: () {
+                BlocProvider.of<AuthBloc>(context).add(const LogOut());
+              },
+              child: Text("Log out", style: Theme.of(context).textTheme.titleLarge!.copyWith(fontWeight: FontWeight.bold, decoration: TextDecoration.underline))
+            )
+          ],
+        ),
+        SizedBox(
+          width: 300,
+          child: Text("You can see and manage your watchlist here", style: Theme.of(context).textTheme.bodyLarge,),
+        )
       ],
     );
   }
