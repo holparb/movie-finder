4388cc07bb53ca46e0706510c9a2527f9c6abae2
Balazs Holpar <balazs02.holpar@gmail.com>
2024-03-18 13:33:12 +0100

    Added getWatchlist datasource test and fixed movie detail datasource tests

--- test/data/datasources/remote/movies_data_source_test.dart
+++ test/data/datasources/remote/movies_data_source_test.dart
@@ -4,6 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 import 'package:movie_finder/config/tmdb_api_config.dart';
 import 'package:movie_finder/core/exceptions/data_error.dart';
+import 'package:movie_finder/core/utils/format_string.dart';
 import 'package:movie_finder/data/datasources/remote/movies_data_source.dart';
 
 import '../../../fixtures/fixture_reader.dart';
@@ -56,36 +57,37 @@ void main() {
     });
   });
   
-  group("get movie details", () { 
+  group("get movie details", () {
+    final String movieDetailsUrlString = createUrlString(formatString(TmdbApiConfig.movieDetailEndpoint, [testMovieDetailModel.id.toString()]));
     test("Should return a valid MovieModel after http call", () async {
       // arrange
-      when(mockClient.get(Uri.parse(createUrlString("${TmdbApiConfig.movieDetailEndpoint}${testMovieDetailModel.id}"))))
+      when(mockClient.get(Uri.parse(movieDetailsUrlString)))
           .thenAnswer((_) async => http.Response(fixture("movie_detail_model.json"), 200));
       // act
       final result = await moviesDataSource.getMovieDetails(testMovieDetailModel.id);
       // assert
       expect(result, testMovieDetailModel);
     });
-  });
 
-  test("Should catch exception if status code is not 200", () async {
-    // arrange
-    when(mockClient.get(Uri.parse(createUrlString("${TmdbApiConfig.movieDetailEndpoint}${testMovieDetailModel.id}"))))
-        .thenAnswer((_) async => http.Response("error", 201));
-    // act
-    final call = moviesDataSource.getMovieDetails(1);
-    // assert
-    expect(() => call, throwsA(isA<DataError>()));
-  });
+    test("Should catch exception if status code is not 200", () async {
+      // arrange
+      when(mockClient.get(Uri.parse(movieDetailsUrlString)))
+          .thenAnswer((_) async => http.Response("error", 201));
+      // act
+      final call = moviesDataSource.getMovieDetails(1);
+      // assert
+      expect(() => call, throwsA(isA<DataError>()));
+    });
 
-  test("Should catch exception if an exception is thrown during fetch", () async {
-    // arrange
-    when(mockClient.get(Uri.parse(createUrlString("${TmdbApiConfig.movieDetailEndpoint}${testMovieDetailModel.id}"))))
-        .thenThrow(Exception("error message"));
-    // act
-    final call = moviesDataSource.getMovieDetails(1);
-    // assert
-    expect(() => call, throwsA(isA<DataError>()));
+    test("Should catch exception if an exception is thrown during fetch", () async {
+      // arrange
+      when(mockClient.get(Uri.parse(movieDetailsUrlString)))
+          .thenThrow(Exception("error message"));
+      // act
+      final call = moviesDataSource.getMovieDetails(1);
+      // assert
+      expect(() => call, throwsA(isA<DataError>()));
+    });
   });
 
   group("Fetch popular movies", () {
@@ -147,6 +149,40 @@ void main() {
       when(mockClient.get(Uri.parse(createUrlString(TmdbApiConfig.topRatedMoviesEndpoint)))).thenThrow(Exception("error message"));
       // act
       final call = moviesDataSource.getTopRatedMovies();
+      // assert
+      expect(() => call, throwsA(isA<DataError>()));
+    });
+  });
+
+  group("Fetch watchlist", () {
+    const String userId = "123";
+    final String watchlistUrlString = "${createUrlString(formatString(TmdbApiConfig.watchListEndpoint, [userId]))}&session_id=$testSessionId";
+    test("Should return a valid MovieModel list after fetching data", () async {
+      // arrange
+      when(mockClient.get(Uri.parse(watchlistUrlString)))
+          .thenAnswer((_) async => http.Response(fixture("movie_list.json"), 200));
+      // act
+      final result = await moviesDataSource.getWatchList(userId, testSessionId);
+      // assert
+      expect(result.length, 3);
+      expect(result, testMovieModels);
+    });
+
+    test("Should return DataError if status code is not 200", () async {
+      // arrange
+      when(mockClient.get(Uri.parse(watchlistUrlString)))
+          .thenAnswer((_) async => http.Response("Something went wrong!", 404));
+      // act
+      final call = moviesDataSource.getWatchList(userId, testSessionId);
+      // assert
+      expect(() => call, throwsA(isA<DataError>()));
+    });
+
+    test("Should return DataError if an exception is thrown during fetch", () async {
+      // arrange
+      when(mockClient.get(Uri.parse(watchlistUrlString))).thenThrow(Exception("error message"));
+      // act
+      final call = moviesDataSource.getWatchList(userId, testSessionId);
       // assert
       expect(() => call, throwsA(isA<DataError>()));
     });
