11351a1af72462541c779b8341b30703f4b75447
Balazs Holpar <balazs02.holpar@gmail.com>
2024-03-18 10:42:20 +0100

    Created empty watchlist widget

--- lib/presentation/widgets/saved_movies_list/saved_movies.dart
+++ lib/presentation/widgets/saved_movies_list/saved_movies.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:movie_finder/domain/entities/movie.dart';
+import 'package:movie_finder/presentation/widgets/saved_movies_list/empty_list.dart';
 import 'package:movie_finder/presentation/widgets/saved_movies_list/saved_movies_header.dart';
 
 class SavedMovies extends StatelessWidget {
-  const SavedMovies({super.key, required this.username});
+  const SavedMovies({super.key, required this.username, required this.watchlist});
 
   final String username;
+  final List<Movie> watchlist;
 
   @override
   Widget build(BuildContext context) {
@@ -12,6 +15,10 @@ class SavedMovies extends StatelessWidget {
       children: [
         SavedMoviesHeader(username: username),
         const SizedBox(height: 16),
+        Expanded(
+            child: watchlist.isEmpty ? const EmptyList() : SizedBox()
+        )
+
       ]
     );
   }
